b4c6b8d2bdbe29e52a747b37fd1aa961b576dd8b
Mandana Dashti <dmandana59@gmail.com>
2025-07-24 21:13:45 +0330

    feat: count occurrences solution

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,25 @@
-function countOccurrences() {}
+//  it's doing it wrong even if it works / msp must return something useful 4ms
+// function countOccurrences(str ,char) {
+//     let count = 0;
+//     str.split('').map((item) => {
+//         if (item===char) {
+//            count++;
+//         }
+//     })
+//     return count;
+// }
+//
+// module.exports = countOccurrences;
+
+// clean code and readable 3ms
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    return count;
+}
 
 module.exports = countOccurrences;
