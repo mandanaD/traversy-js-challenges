7922b864dbac3f1398eb6fb02f55ac7c4910de7d
Mandana Dashti <dmandana59@gmail.com>
2025-07-24 21:14:14 +0330

    feat: find max solution

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,14 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+    // build in js method
+    // return Math.max(...arr);
+
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
 
 module.exports = findMaxNumber;
