3e16603f996d670e6c4cddbb03e27fffe6186249
Mandana Dashti <dmandana59@gmail.com>
2025-07-24 21:13:20 +0330

    feat: calculator solution

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,21 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+    let result ;
+    switch (operator) {
+        case "+":
+            result= num1 + num2;
+            break;
+        case "-":
+            result= num1 - num2;
+            break;
+        case "*":
+            result= num1 * num2;
+            break;
+        case "/":
+            result= num1 / num2;
+            break;
+        default: throw new Error('invalid operator');
+    }
+    return result;
+}
 
 module.exports = calculator;
