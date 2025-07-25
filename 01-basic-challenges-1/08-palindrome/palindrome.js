101caf1c3414d67e377235bb92cd7c99bb412d78
Mandana Dashti <dmandana59@gmail.com>
2025-07-25 15:55:52 +0330

    feat: palindrome solution

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,7 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+    let formatedStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedStr = formatedStr.split("").reverse().join("")
+    return reversedStr === formatedStr;
+}
 
 module.exports = isPalindrome;
