c6d593ff1b133b3d10be247f54308eef7b305da3
Mandana Dashti <dmandana59@gmail.com>
2025-07-25 15:55:19 +0330

    feat: count vowels solution

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(str) {
+    const vowels = ['a', 'o', 'u', 'i', 'e'];
+    str = str.toLowerCase();
+    let vowelsCount = 0
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            vowelsCount++
+        }
+    }
+    return vowelsCount;
+}
 
 module.exports = countVowels;
