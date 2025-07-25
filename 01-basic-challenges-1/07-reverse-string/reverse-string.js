8d285a8481fcd352b7fea6ca3ca3e9928ad61adc
Mandana Dashti <dmandana59@gmail.com>
2025-07-25 15:56:43 +0330

    feat: reverse string solution

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,9 @@
-function reverseString() {}
+function reverseString(str) {
+    let reverseString = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseString += str[i];
+    }
+    return reverseString;
+}
 
 module.exports = reverseString;
