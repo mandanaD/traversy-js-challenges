076542edd43ba367a62bf9ca03ce5947239d9013
Mandana Dashti <dmandana59@gmail.com>
2025-07-26 22:44:39 +0330

    feat: find missing num solution

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+// function findMissingNumber(arr) {
+//     const sortedArr = arr.sort((a, b) => a - b);
+//
+//     if (sortedArr.length) {
+//         for (let i = 0; i < sortedArr.length; i++) {
+//             if (sortedArr[i] !== i + 1) {
+//                 return i + 1;
+//             }
+//         }
+//     } else return 1;
+// }
+
+function findMissingNumber(arr) {
+    const n = arr.length + 1;
+    const expectedSum = n * (n + 1) / 2
+    const actualSum = arr.reduce((sum, num) => sum + num, 0);
+    return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
