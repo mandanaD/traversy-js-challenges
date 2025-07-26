a5f86fd240dce9174d626fbe80fe9630fc09e59b
Mandana Dashti <dmandana59@gmail.com>
2025-07-26 22:44:53 +0330

    feat: fizzBuzz solution

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const fizzBuzzArray = []
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzBuzzArray.push("FizzBuzz");
+        } else if (i % 5 === 0) {
+            fizzBuzzArray.push("Buzz");
+        } else if (i % 3 === 0) {
+            fizzBuzzArray.push("Fizz");
+        } else {
+            fizzBuzzArray.push(i);
+        }
+    }
+    return fizzBuzzArray;
+}
 
 module.exports = fizzBuzzArray;
