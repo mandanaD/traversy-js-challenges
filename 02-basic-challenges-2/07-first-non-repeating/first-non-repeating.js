bbfd50e9da3a81402b76d8d9998f5e046f961497
Mandana Dashti <dmandana59@gmail.com>
2025-07-28 23:50:45 +0330

    feat: non repeating solution

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,14 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+    const charCount = {}
+    for (const strElement of str) {
+        charCount[strElement] = (charCount[strElement] || 0) + 1;
+    }
+    for (const strElement of str) {
+        if (charCount[strElement] === 1) {
+            return strElement;
+        }
+    }
+    return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
