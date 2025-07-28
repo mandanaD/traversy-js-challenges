19f54bbacb6ba81de1b5c19e59ac2d5e05c46cc2
Mandana Dashti <dmandana59@gmail.com>
2025-07-28 23:49:37 +0330

    feat: all chars solution

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,25 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//     const charArray = []
+//     for (let i = 0; i < str.length; i++) {
+//         if (charArray.includes(str[i])) {
+//             return false
+//         }
+//         charArray.push(str[i]);
+//     }
+//     return true
+// }
+
+
+function areAllCharactersUnique(str) {
+    const charSet=new Set();
+    for (let i = 0; i < str.length; i++) {
+        if (charSet.has(str[i])) {
+            return false
+        }else {
+            charSet.add(str[i]);
+        }
+    }
+    return true;
+}
 
 module.exports = areAllCharactersUnique;
