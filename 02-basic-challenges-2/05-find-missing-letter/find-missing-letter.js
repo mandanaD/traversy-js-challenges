df0b23c56f2d570ec2362683eb64e5582f6a546a
Mandana Dashti <dmandana59@gmail.com>
2025-07-28 23:49:52 +0330

    feat: missing letter solution

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,12 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const firstIndex = alphabet.indexOf(arr[0]);
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== alphabet[firstIndex + i]) {
+            return alphabet[firstIndex + i];
+        }
+    }
+    return "";
+}
 
 module.exports = findMissingLetter;
