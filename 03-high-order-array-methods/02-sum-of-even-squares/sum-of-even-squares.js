3efcf7ed3867064a20ed431c35cc87f58e016afb
Mandana Dashti <dmandana59@gmail.com>
2025-08-02 21:34:41 +0330

    feat: sum of squares solution

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,10 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+    const evenSquares = numbers
+        .filter(number => number % 2 === 0)
+        .map(number => number ** 2)
+        .reduce((a, b) => a + b, 0);
+    if (evenSquares) return evenSquares;
+    return 0;
+}
 
 module.exports = sumOfEvenSquares;
