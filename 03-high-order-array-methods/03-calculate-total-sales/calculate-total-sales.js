f26d2e8a59c5bfc0489424e731ecc6c6289e2cd7
Mandana Dashti <dmandana59@gmail.com>
2025-08-02 21:33:51 +0330

    feat: calculate total solution

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,7 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+    const productsPrice = products.reduce((acc, product) => acc + (product.price * product.quantity), 0);
+    const taxPrice = (productsPrice * taxRate) / 100;
+    return parseFloat(productsPrice + taxPrice).toFixed(2);
+}
 
 module.exports = calculateTotalSalesWithTax;
