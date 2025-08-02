beac16e35ce2c68e05871d3e0686c0a08d11f36b
Mandana Dashti <dmandana59@gmail.com>
2025-08-02 21:34:24 +0330

    feat: format phone solution

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,6 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+    const numbersToString=numbers.join("")
+    return `(${numbersToString.substring(0, 3)}) ${numbersToString.substring(3, 6)}-${numbersToString.substring(6)}`
+}
 
 module.exports = formatPhoneNumber;
