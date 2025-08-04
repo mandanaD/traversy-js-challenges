4a2b0d4bbfbb343ad3541cc8eb554446b48784fe
Mandana Dashti <dmandana59@gmail.com>
2025-08-04 23:41:48 +0330

    feat: valid ip solution

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,7 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+    const str1Score = str1.split("").reduce((acc, letter) => acc + letter.charCodeAt(0) - 96, 0);
+    const str2Score = str2.split("").reduce((acc, letter) => acc + letter.charCodeAt(0) - 96, 0);
+    return str1Score === str2Score;
+}
 
 module.exports = validAnagrams;
--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,12 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (string) => {
+    const seperatedString = string.split(".");
+
+    if (seperatedString.length !== 4) return false;
+
+    return seperatedString.every((item) => {
+        const num = parseInt(item);
+        return (num >= 0 && num <= 255 && num.toString() === item)
+    })
+};
 
 module.exports = isValidIPv4;
