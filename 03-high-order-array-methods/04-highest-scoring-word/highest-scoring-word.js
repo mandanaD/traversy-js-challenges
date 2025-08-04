f8221663ca9c09704233ed9cfde6f3e9b7893529
Mandana Dashti <dmandana59@gmail.com>
2025-08-04 23:41:35 +0330

    feat: hashing solution

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,24 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+    const words = str.split(' ');
+
+    const scores = words.map((word) => {
+        let score = 0;
+        for (const letter of word) {
+            score += letter.toLowerCase().charCodeAt(0) - 96;
+        }
+        return score;
+    })
+
+    let highestScore = 0;
+    let highestScoreIndex = 0;
+    for (let i = 0; i < scores.length; i++) {
+        if (highestScore < scores[i]) {
+            highestScore = scores[i];
+            highestScoreIndex = i
+        }
+    }
+
+    return words[highestScoreIndex];
+}
 
 module.exports = highestScoringWord;
--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,11 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+    if (str.trim() === '') {
+        return false
+    }
+    let words = str.trim().split(/\s+/).map((item) => item[0].toUpperCase() + item.slice(1))
+    if (words.join('').length  > 140) return false
+
+    return "#" + words.join('')
+}
 
 module.exports = generateHashtag;
