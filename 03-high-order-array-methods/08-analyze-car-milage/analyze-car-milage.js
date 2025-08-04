9cb3c288053cc481f903a64b1759bb208eafdfe2
Mandana Dashti <dmandana59@gmail.com>
2025-08-04 23:38:05 +0330

    feat: analyze solution

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,14 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+    let totalMileage = cars.reduce((acc, car) => (acc + car.mileage), 0);
+    let averageMileage = (totalMileage / cars.length).toFixed(2)
+    let highestMileageCar = cars.reduce((highest, car) => (highest.mileage > car.mileage ? highest : car), cars[0]);
+    let lowestMileageCar = cars.reduce((highest, car) => (highest.mileage < car.mileage ? highest : car), cars[0]);
+    return {
+        totalMileage,
+        averageMileage:parseFloat(averageMileage),
+        highestMileageCar,
+        lowestMileageCar,
+    }
+}
 
 module.exports = analyzeCarMileage;
